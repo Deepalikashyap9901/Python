bac1cdfde4a0760ac46189455e79c36a9f6ca26c
Riya Kashyap <78717983+Deepalikashyap9901@users.noreply.github.com>
2021-02-26 07:48:25 +0530

    love with python  new project
    I you like this project

--- main.py
+++ main.py
@@ -1,60 +1,20 @@
-import pygame
-
-pygame.init()
-window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('track6.png')
-car = pygame.image.load('tesla.png')
-car = pygame.transform.scale(car, (30, 60))
-car_x = 155
-car_y = 300
-focal_dis = 25
-cam_x_offset = 0
-cam_y_offset = 0
-direction = 'up'
-drive = True
-clock = pygame.time.Clock()
-while drive:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            drive = False
-    clock.tick(60)
-    cam_x = car_x + cam_x_offset + 15
-    cam_y = car_y + cam_y_offset + 15
-    up_px = window.get_at((cam_x, cam_y - focal_dis))[0]
-    down_px = window.get_at((cam_x, cam_y + focal_dis))[0]
-    right_px = window.get_at((cam_x + focal_dis, cam_y))[0]
-    print(up_px, right_px, down_px)
-
-    # change direction (take turn)
-    if direction == 'up' and up_px != 255 and right_px == 255:
-        direction = 'right'
-        cam_x_offset = 30
-        car = pygame.transform.rotate(car, -90)
-    elif direction == 'right' and right_px != 255 and down_px == 255:
-        direction = 'down'
-        car_x = car_x + 30
-        cam_x_offset = 0
-        cam_y_offset = 30
-        car = pygame.transform.rotate(car, -90)
-    elif direction == 'down' and down_px != 255 and right_px == 255:
-        direction = 'right'
-        car_y = car_y + 30
-        cam_x_offset = 30
-        cam_y_offset = 0
-        car = pygame.transform.rotate(car, 90)
-    elif direction == 'right' and right_px != 255 and up_px == 255:
-        direction = 'up'
-        car_x = car_x + 30
-        cam_x_offset = 0
-        car = pygame.transform.rotate(car, 90)
-    # drive
-    if direction == 'up' and up_px == 255:
-        car_y = car_y - 2
-    elif direction == 'right' and right_px == 255:
-        car_x = car_x + 2
-    elif direction == 'down' and down_px == 255:
-        car_y = car_y + 2
-    window.blit(track, (0, 0))
-    window.blit(car, (car_x, car_y))
-    pygame.draw.circle(window, (0, 255, 0), (cam_x, cam_y), 5, 5)
-    pygame.display.update()
+from turtle import *
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x/100 + 1)
+    t.forward(x)
+    t.left(59)
+color("red", )
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40, 180)
+forward(100)
+end_fill()
+done()
